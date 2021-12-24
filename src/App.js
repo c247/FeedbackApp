9a655d1c0700f2449c829514f508f783aa71caa5
Vijay <sccr247@gmail.com>
2021-12-24 12:30:01 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Card from "./components/shared/Card";
+import { v4 as uuidv4 } from "uuid";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContent";
+import AboutIconLink from "./components/AboutIconLink";
+
+function App() {
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header text={"Feedback UI"} />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+          </Routes>
+          <Routes>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  );
+}
+
+export default App;
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,28 @@
+import { FaTimes, FaEdit } from "react-icons/fa";
+import { useContext } from "react";
+import { useState } from "react";
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContent";
+
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+
+  const handleClick = (id) => {
+    console.log(id);
+  };
+  return (
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <button onClick={() => editFeedback(item)} className="edit">
+        <FaEdit color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
+}
+
+export default FeedbackItem;
